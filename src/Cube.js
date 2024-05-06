a352d49cb44ea11e50620389a5428ceb6faf35b4
golddash <golddashk@gmail.com>
2024-05-05 19:49:38 -0700

    added 1 texture

--- src/Cube.js
+++ src/Cube.js
@@ -6,76 +6,128 @@ class Cube {
     //this.size = 5.0;
     //this.segments = 10;
     this.matrix = new Matrix4();
+    this.textureNum=-1;
   }
+
+  // render() {
+  //   //var xy = this.position;
+  //   var rgba = this.color;
+  //   //var size = this.size;
+
+  //   gl.uniform1i(u_whichTexture, this.textureNum);
+
+  //   gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+  //   //gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+  //   gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+  //   // front of cube
+
+  //   drawTriangle3DUV( [0,0,0, 1,1,0, 1,0,0], [ 0,0, 1,1, 1,0]);
+  //   // drawTriangle3DUV( [0,0,0, 0,1,0, 1,1,0], [ 0,0, 0,1, 1,1]);
+
+  //   drawTriangle3D([0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0]);
+  //   drawTriangle3D([0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0]);
+
+  //   gl.uniform4f(
+  //     u_FragColor,
+  //     rgba[0] * 0.9,
+  //     rgba[1] * 0.9,
+  //     rgba[2] * 0.9,
+  //     rgba[3]
+  //   );
+
+  //   // top of cube
+  //   drawTriangle3D([0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
+  //   drawTriangle3D([0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0]);
+
+  //   // // Fake Lighting
+  //   // gl.uniform4f(
+  //   //   u_FragColor,
+  //   //   rgba[0] * 0.9,
+  //   //   rgba[1] * 0.9,
+  //   //   rgba[2] * 0.9,
+  //   //   rgba[3]
+  //   // );
+
+  //   // Draw back of the cube
+  //   drawTriangle3D([0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0]);
+  //   drawTriangle3D([0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
+
+  //   // Fake lighting
+  //   gl.uniform4f(
+  //     u_FragColor,
+  //     rgba[0] * 0.8,
+  //     rgba[1] * 0.8,
+  //     rgba[2] * 0.8,
+  //     rgba[3]
+  //   );
+
+  //   // Fake lighting
+  //   gl.uniform4f(
+  //     u_FragColor,
+  //     rgba[0] * 0.7,
+  //     rgba[1] * 0.7,
+  //     rgba[2] * 0.7,
+  //     rgba[3]
+  //   );
+
+  //   // Draw bottom of the cube
+  //   drawTriangle3D([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0]);
+  //   drawTriangle3D([0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0]);
+
+  //   // Draw left side of cube
+  //   drawTriangle3D([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0]);
+  //   drawTriangle3D([0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]);
+
+  //   // Draw right side of cube
+  //   drawTriangle3D([1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0]);
+  //   drawTriangle3D([1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0]);
+  // }
 
   render() {
     //var xy = this.position;
     var rgba = this.color;
     //var size = this.size;
-
+    //console.log(this.textureNum);
+    gl.uniform1i(u_whichTexture, this.textureNum);
+    // Pass the color of a point to u_FragColor variable
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
 
-    //gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-    // front of cube
 
-    drawTriangle3D([0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0]);
-    drawTriangle3D([0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0]);
 
-    gl.uniform4f(
-      u_FragColor,
-      rgba[0] * 0.9,
-      rgba[1] * 0.9,
-      rgba[2] * 0.9,
-      rgba[3]
-    );
+    //front of cubes
+    drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0], [0,0, 1,1, 1,0]);
+    drawTriangle3DUV([0,0,0, 0,1,0, 1,1,0], [0,0, 0,1, 1,1]);
 
-    // top of cube
-    drawTriangle3D([0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
-    drawTriangle3D([0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0]);
+    //back of cube
+    drawTriangle3DUV([0,0,1, 1,1,1, 1,0,1], [1,0, 0,1, 0,0]);
+    drawTriangle3DUV([0,0,1, 0,1,1, 1,1,1], [1,0, 1,1, 0,1]);
 
-    // // Fake Lighting
-    // gl.uniform4f(
-    //   u_FragColor,
-    //   rgba[0] * 0.9,
-    //   rgba[1] * 0.9,
-    //   rgba[2] * 0.9,
-    //   rgba[3]
-    // );
 
-    // Draw back of the cube
-    drawTriangle3D([0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0]);
-    drawTriangle3D([0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
+    gl.uniform4f(u_FragColor, rgba[0] * 0.9, rgba[1] * 0.9, rgba[2] * 0.9, rgba[3]);
 
-    // Fake lighting
-    gl.uniform4f(
-      u_FragColor,
-      rgba[0] * 0.8,
-      rgba[1] * 0.8,
-      rgba[2] * 0.8,
-      rgba[3]
-    );
+    //top of cube
+    drawTriangle3DUV([0,1,0, 0,1,1, 1,1,1], [0,0, 0,1, 1,1]);
+    drawTriangle3DUV([0,1,0, 1,1,1, 1,1,0], [0,0, 1,1, 1,0]);
 
-    // Fake lighting
-    gl.uniform4f(
-      u_FragColor,
-      rgba[0] * 0.7,
-      rgba[1] * 0.7,
-      rgba[2] * 0.7,
-      rgba[3]
-    );
 
-    // Draw bottom of the cube
-    drawTriangle3D([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0]);
-    drawTriangle3D([0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0]);
+    //bottom
+    drawTriangle3DUV([0,0,0, 0,0,1, 1,0,1], [0,1, 0,0, 1,0]);
+    drawTriangle3DUV([0,0,0, 1,0,1, 1,0,0], [0,1, 1,0, 1,1]);
 
-    // Draw left side of cube
-    drawTriangle3D([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0]);
-    drawTriangle3D([0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]);
 
-    // Draw right side of cube
-    drawTriangle3D([1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0]);
-    drawTriangle3D([1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0]);
+    gl.uniform4f(u_FragColor, rgba[0] * 0.8, rgba[1] * 0.8, rgba[2] * 0.8, rgba[3]);
+
+    //right side of cube
+    drawTriangle3DUV([1,0,0, 1,1,0, 1,1,1], [0,0, 0,1, 1,1]);
+    drawTriangle3DUV([1,0,0, 1,1,1, 1,0,1], [0,0, 1,1, 1,0]);
+
+    //left side of cube
+    drawTriangle3DUV([0,0,0, 0,1,0, 0,1,1], [1,0, 1,1, 0,1]);
+    drawTriangle3DUV([0,0,0, 0,1,1, 0,0,1], [1,0, 0,1, 0,0]);
+
   }
 }
--- src/World.js
+++ src/World.js
@@ -1,20 +1,58 @@
 // Kevin Chen 4/28/24
 
 var VSHADER_SOURCE = `
+  precision mediump float;
   attribute vec4 a_Position;
+  attribute vec2 a_UV;
+  varying vec2 v_UV;
   uniform mat4 u_ModelMatrix;
   uniform mat4 u_GlobalRotateMatrix;
+  uniform mat4 u_ViewMatrix;
+  uniform mat4 u_ProjectionMatrix;
   void main() {
-    gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+    v_UV = a_UV;
    }`;
 
 // Fragment shader program
 var FSHADER_SOURCE = `
   precision mediump float;
   uniform vec4 u_FragColor;
+  varying vec2 v_UV;
+  uniform sampler2D u_Sampler0;
+  uniform int u_whichTexture;
   void main() {
-    gl_FragColor = u_FragColor;
+    if (u_whichTexture == -2) {
+      gl_FragColor = u_FragColor;
+    }
+    else if (u_whichTexture == -1) {
+      gl_FragColor = vec4(v_UV, 1.0, 1.0);
+    }
+    else if (u_whichTexture == 0) {
+      gl_FragColor = texture2D(u_Sampler0, v_UV);
+    } else {
+      gl_FragColor = vec4(1, 0.2, 0.2, 1.0);
+    }
+    
   }`;
+
+
+// var VSHADER_SOURCE = `
+//   attribute vec4 a_Position;
+//   uniform mat4 u_ModelMatrix;
+//   uniform mat4 u_GlobalRotateMatrix;
+//   void main() {
+//     gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+//    }`;
+
+// // Fragment shader program
+// var FSHADER_SOURCE = `
+//   precision mediump float;
+//   uniform vec4 u_FragColor;
+//   void main() {
+//     gl_FragColor = u_FragColor;
+//   }`;
+
 
 let canvas;
 let gl;
@@ -23,6 +61,12 @@ let u_FragColor;
 let u_Size;
 let u_ModelMatrix;
 let u_GlobalRotateMatrix;
+
+let a_UV;
+let u_ProjectionMatrix;
+let u_ViewMatrix;
+let u_Sampler0;
+let u_whichTexture;
 
 function setupWebGL() {
   // Retrieve <canvas> element
@@ -71,6 +115,36 @@ function connectVariablesToGLSL() {
   );
   if (!u_GlobalRotateMatrix) {
     console.log("Failed to get the storage location of u_GlobalRotateMatrix");
+    return;
+  }
+
+  a_UV = gl.getAttribLocation(gl.program, "a_UV");
+  if (a_UV < 0) {
+    console.log("Failed to get the storage location of a_UV");
+    return;
+  }
+
+  u_ViewMatrix = gl.getUniformLocation(gl.program, "u_ViewMatrix");
+  if (!u_ViewMatrix) {
+    console.log("Failed to get the storage location of u_ViewMatrix");
+    return;
+  }
+
+  u_ProjectionMatrix = gl.getUniformLocation(gl.program, "u_ProjectionMatrix");
+  if (!u_ProjectionMatrix) {
+    console.log("Failed to get the storage location of u_ProjectionMatrix");
+    return;
+  }
+
+  u_Sampler0 = gl.getUniformLocation(gl.program, "u_Sampler0");
+  if (!u_Sampler0) {
+    console.log("Failed to get the storage location of u_Sampler0");
+    return;
+  }
+
+  u_whichTexture = gl.getUniformLocation(gl.program, "u_whichTexture");
+  if (!u_whichTexture) {
+    console.log("Failed to get the storage location of u_whichTexture");
     return;
   }
 
@@ -247,6 +321,42 @@ function addActionsForHtmlUI() {
 
 }
 
+function initTextures() {
+  var image = new Image();
+  if (!image) {
+    console.log("Failed to create the image object");
+    return false;
+  }
+
+  image.onload = function() {sendImageToTEXTURE0 (image); };
+  image.src = "../images/car.jpg";
+
+  console.log('created image0');
+
+  return true;
+}
+
+function sendImageToTEXTURE0 (image) {
+  var texture = gl.createTexture();
+  if (!texture) {
+    console.log("Failed to create the texture object");
+    return false;
+  
+  }
+
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.uniform1i(u_Sampler0, 0);
+  console.log('finished loadTexture');
+}
+
+
+
+
+
 function main() {
   setupWebGL();
 
@@ -264,11 +374,17 @@ function main() {
     }
   };
 
+  initTextures();
+
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //renderAllShapes();
+
+  requestAnimationFrame(tick);
 }
 
 var g_shapeList = [];
@@ -293,7 +409,7 @@ function click(ev) {
   g_shapeList.push(point);
 
   //renderAllShapes();
-  requestAnimationFrame(tick);
+  
 }
 
 function convertCoordinatesEventToGL(ev) {
@@ -313,7 +429,7 @@ var g_seconds = performance.now()/1000.0 - g_startTime;
 function tick() {
 
   g_seconds = performance.now()/1000.0 - g_startTime;
-  console.log(g_seconds);
+  //console.log(g_seconds);
   updateAnimationAngles();
   renderAllShapes();
   requestAnimationFrame(tick);
@@ -350,6 +466,14 @@ function updateAnimationAngles()
 function renderAllShapes() {
   var startTime = performance.now();
 
+  var projMat = new Matrix4();
+  projMat.setPerspective(90, canvas.width / canvas.height, .1, 100);
+  gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
+
+  var viewMat = new Matrix4();
+  viewMat.setLookAt(0,0,-1, 0,0,0, 0,1,0);
+  gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
+
   var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
 
   globalRotMat.rotate(g_globalAngleX, 1, 0, 0);
@@ -377,6 +501,7 @@ function renderAllShapes() {
   // Front of Fox Body
   var foxFront = new Cube();
   foxFront.color = [0.95, 0.8, 0.6, 1.0];
+  foxFront.textureNum = 0;
   foxFront.matrix.translate(0, -0.6, -0.20);
   foxFront.matrix.rotate(0, 1, 0, 0);
   foxFront.matrix.rotate(0, 0, 0, 1);
@@ -388,6 +513,7 @@ function renderAllShapes() {
   // Head of Fox
   var foxHead = new Cube();
   foxHead.color = [0.6,0.3,0.1,1];
+  // foxHead.textureNum = 2;
   foxHead.matrix = attachHead;
   foxHead.matrix.translate(0, .70, -0.05);
   foxHead.matrix.rotate(0, 0, 0, 1);
